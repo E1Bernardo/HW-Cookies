b59c0177f626cbf791385e0f0459f3c5155d68bc
E1Bernardo <goonypony@gmail.com>
2023-07-31 20:06:05 +0500

    v3.1

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@ deleteBtn.style.cursor = 'none'
 if(document.cookie == ''){
     circleCount = 3;
     createStartCircle(circleCount);
-    document.cookie = "current=3";
+    document.cookie = `current=3;max-age=${24*60*60}`;
 }
 
 document.getElementById('add-circle').addEventListener('click', () =>{

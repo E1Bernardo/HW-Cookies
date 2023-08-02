777073534d3fee08b9dcd8a1ca0e4d2c788b538c
E1Bernardo <goonypony@gmail.com>
2023-08-02 08:50:52 +0500

    V4

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,20 @@
 var circleCount = 0;
 const container = document.querySelector('.circle-container');
 const deleteBtn = document.getElementById('del-circle');
-deleteBtn.style.backgroundColor = '#4343437e';
-deleteBtn.style.cursor = 'none'
+
+if(getCookie('current') == '0'){
+    deleteBtn.style.backgroundColor = '#4343437e';
+    deleteBtn.style.cursor = 'none'
+}
 
 if(document.cookie == ''){
     circleCount = 3;
     createStartCircle(circleCount);
     document.cookie = `current=3;max-age=${24*60*60}`;
+}
+if(document.cookie != ''){
+    circleCount = getCookie('current');
+    createStartCircle(circleCount);
 }
 
 document.getElementById('add-circle').addEventListener('click', () =>{
@@ -16,13 +23,13 @@ document.getElementById('add-circle').addEventListener('click', () =>{
     container.appendChild(element);
     deleteBtn.style.backgroundColor = 'white';
     deleteBtn.style.cursor = 'pointer'
-    document.cookie = `current=3`;
-    console.log(document.cookie);
+    document.cookie = `current=${++circleCount}`;
 });
 
 document.getElementById('del-circle').addEventListener('click', () =>{
     if(container.children.length != 0){
         var circles = container.lastChild.remove();
+        document.cookie = `current=${--circleCount}`;
     }
     if(container.children.length == 0){
         deleteBtn.style.backgroundColor = '#4343437e';
@@ -38,4 +45,12 @@ function createStartCircle(count){
     }
 }
 
-
+function getCookie(key){
+    let cookies = {};
+    let tempCookiesStrings = document.cookie.split('; ');
+    for(let el of tempCookiesStrings){
+        let pare = el.split('=');
+        cookies[pare[0]] = pare[1];
+    }
+    return cookies[key];
+}
